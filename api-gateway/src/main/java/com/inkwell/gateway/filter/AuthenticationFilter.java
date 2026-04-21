778a4084ed3ddecb82ea30943b9bbc6c0446a264
Aditya <ar850732@gmail.com>
2026-04-22 00:06:01 +0530

    [Aditya raj] Add. Added Api gateway

---
+++ api-gateway/src/main/java/com/inkwell/gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.inkwell.gateway.filter;
+
+import com.inkwell.gateway.config.RouteValidator;
+import com.inkwell.gateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {}
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+                // Check if Header contains token
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("Missing Authorization Header");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+                
+                try {
+                    // Validate Token
+                    jwtUtil.validateToken(authHeader);
+                } catch (Exception e) {
+                    throw new RuntimeException("Unauthorized access to application");
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+}
